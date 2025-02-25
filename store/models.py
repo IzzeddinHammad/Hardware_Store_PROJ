2240d59057b795c4482d32dc7b9422d67ee7964f
X00219608 <X00219608@mytudublin.ie>
2025-02-25 17:33:57 +0000

    creating the super user and creating the store model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="images/products/",null=True, blank=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2,default=0.00)
+    stock = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name="reviews")
+    # user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating = models.PositiveBigIntegerField(default=3)
+    comment = models.TextField()
+    # creation_time = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.comment
