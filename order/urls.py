58cfe5ffb8908650f175b2531387225068797136
MMcGrane <markmcgrane@hotmail.com>
2025-03-13 20:18:53 +0000

    Finished the order page

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import process_payment, payment_success, payment_failure
+from . import views
 
 urlpatterns = [
     path('pay/<int:order_id>/', process_payment, name='process_payment'),
     path('payment_success/', payment_success, name='payment_success'),
     path('payment_failure/', payment_failure, name='payment_failure'),
+    path('<int:order_id>/', views.orderDetail.as_view(), name='order_detail'),
+    #path('history/', views.orderHistory.as_view(), name='order_history'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,7 +1,7 @@
 import stripe
 from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Order
+from .models import Order, OrderItem
 from cart.models import Cart, CartItem
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
@@ -14,6 +14,15 @@ class orderHistory(LoginRequiredMixin, View):
             email =str(request.user.email)
             order_details = Order.objects.filter(emailAddress=email)
         return render(request, 'order_list.html', {'order_details': order_details})
+
+class orderDetail(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        if request.user.is_authenticated:
+            email = str(request.user.email)
+            order = Order.objects.get(id=order_id, emailAddress=email)
+            order_items = OrderItem.objects.filter(order=order)
+
+        return render(request, 'order_detail.html', {'order': order, 'order_items': order_items})
 
 
 def process_payment(request, order_id):
